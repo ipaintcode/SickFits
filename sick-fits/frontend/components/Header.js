6c8eed1af1e5d9f801e39e15d45dd8e8aa88c34a
Mark Learst <learst@me.com>
2018-10-29 19:03:17 -0400

    Added ssr CSS rendering flash of unstyled content.

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,19 @@
-import Nav from './Nav'
 import Link from 'next/link'
 import styled from 'styled-components'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+import Nav from './Nav'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
+Router.onRouteChangeError = () => {
+  NProgress.done()
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -43,13 +56,11 @@ const StyledHeader = styled.header`
 const Header = () => (
   <StyledHeader>
     <div className="bar">
-      <h1>
-        <Logo>
-          <Link href="/">
-            <a>Sick Fits</a>
-          </Link>
-        </Logo>
-      </h1>
+      <Logo>
+        <Link href="/">
+          <a>Sick Fits</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
