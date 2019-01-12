0f4fa03aebe128a6a85238a3f76fc3e108255fa8
Mark Learst <learst@me.com>
2019-01-12 03:44:35 -0500

    Base styledcomponents for Header, Page and sell.

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
 import Meta from '../components/Meta'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
+
+const theme = {
+  red: '#ff0000',
+  black: '#393939',
+  grey: '#3a3a3a',
+  lightgrey: '#e1e1e1',
+  offWhite: '#ededed',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
